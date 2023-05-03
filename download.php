30d3065d871f52d2ebd777b509e37039e77454d7
Miguel Carlo Flores <95689181+MFlores01@users.noreply.github.com>
2023-05-03 11:10:53 +0800

    Add files via upload

---
+++ download.php
@@ -0,0 +1,56 @@
+<?php
+
+require "vendor/autoload.php";
+
+session_start();
+
+use App\QuestionManager;
+
+$score = null;
+$answers = [];
+$results = [];
+$i = 1;
+
+try {
+    $manager = new QuestionManager;
+    $manager->initialize();
+
+    if (!isset($_SESSION['answers'])) {
+        throw new Exception('Missing answers');
+    }
+
+    $answers = $_SESSION['answers'];
+    $score = $manager->computeScore($answers);
+    $results = $manager->results($answers);
+
+    $file = "results.txt";
+    $txt = fopen($file, "w") or die("Unable to open file!");
+
+    $output = "Complete Name: ".$_SESSION['complete_name']."\n";
+    $output .= "Email: ".$_SESSION['email']."\n";
+    $output .= "Birthdate: ".$_SESSION['birthdate']."\n";
+    $output .= "Score: ".$score." out of ".$manager->getQuestionSize()."\n";
+    $output .= "Answers:\n\n";
+
+    
+foreach ($results as $answer) {
+    if ($answer[1] == 1) {
+        $output .= $i . ") " . $answer[0] . " (correct) \n";
+    } else {
+        $output .= $i . ") " . $answer[0] . " (incorrect) \n";
+    }
+    $i++;
+}
+    
+    
+    header('Content-Disposition: attachment; filename='.basename($file));
+    fwrite($txt, $output);
+    fclose($txt);
+    readfile($file);
+    exit;
+
+} catch (Exception $e) {
+    echo '<h1>An error occurred:</h1>';
+    echo '<p>' . $e->getMessage() . '</p>';
+    exit;
+}
